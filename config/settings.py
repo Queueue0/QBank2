f1c71d12d1499c8a40a5c1a7d704f9e11fb312af
Queueue0 <jcoffey97@gmail.com>
2021-09-13 22:51:52 -0400

    fixed minor typo

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), con_max_age=600)
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
     }
 
 
