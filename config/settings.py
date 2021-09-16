fefe75d7bde7e2bf22d0425e30f4861f1dcd44d6
Queueue0 <jcoffey97@gmail.com>
2021-09-16 00:43:38 -0400

    added sentry support to better track issues in production

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,24 @@ import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 load_dotenv(override=True)
+
+sentry_sdk.init(
+    dsn=os.getenv('DSN'),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
