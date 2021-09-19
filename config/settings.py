d47a7f6bd77e8ac6b783e166e71103495bb0d9c4
Queueue0 <jcoffey97@gmail.com>
2021-09-19 12:49:33 -0400

    sentry will now only send me emails if something goes wrong in production

--- config/settings.py
+++ config/settings.py
@@ -19,20 +19,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv(override=True)
 
-sentry_sdk.init(
-    dsn=os.getenv('DSN'),
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,6 +30,22 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
+
+if (not DEBUG):
+    sentry_sdk.init(
+        dsn=os.getenv('DSN'),
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+
 
 ALLOWED_HOSTS = os.getenv('HOSTS').split(',')
 
